d1a68210c968292db02bbf205f3be2f1e0785997
floaty Horizon <floaty.horizonofficial@gmail.com>
2024-11-15 18:35:26 +0100

    Why not add everything

---
+++ lua/chadrc.lua
@@ -0,0 +1,21 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.term = {
+	sizes = {sp=0.3,vsp=0.4}
+}
+
+M.base46 = {
+	theme = "bearded-arc",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
---
+++ lua/mappings.lua
@@ -0,0 +1,8 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+-- local map = vim.keymap.set
+
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
